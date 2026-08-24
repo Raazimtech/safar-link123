dc11d5cb70e32349f84fda9f74a5f2e82e31981e
Raazim tech <raazimtech@gmail.com>
2026-08-24 11:44:29 +0300

    Add EduX service worker

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'edux-admin-v1';
+const CORE = ['./','./index.html','./manifest.webmanifest','./favicon.svg','./icons/icon-192.svg','./icons/icon-512.svg','./pwa.js'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(fetch(request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match(request).then(cached => cached || caches.match('./index.html'))));
+});
