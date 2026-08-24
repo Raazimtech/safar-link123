1e154622ca1c8c968ff03ad3c09e8d160934ece9
Raazim tech <raazimtech@gmail.com>
2026-08-24 12:00:57 +0300

    Refresh EDXAD PWA cache and service worker version

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE = 'edux-admin-v3';
+const CACHE = 'edux-admin-v4';
 const CORE = [
-  './', './index.html', './manifest.webmanifest', './favicon.svg', './pwa.js',
-  './icons/icon-192.png', './icons/icon-512.png', './icons/icon-512-maskable.png'
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './favicon.svg',
+  './pwa.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(cache => cache.addAll(CORE))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-    )).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key.startsWith('edux-admin-') && key !== CACHE)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
@@ -25,27 +36,34 @@ self.addEventListener('message', event => {
 self.addEventListener('fetch', event => {
   const request = event.request;
   if (request.method !== 'GET') return;
+
   const url = new URL(request.url);
   if (url.origin !== self.location.origin) return;
 
   if (request.mode === 'navigate') {
     event.respondWith(
-      fetch(request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put('./index.html', copy)).catch(() => {});
-        return response;
-      }).catch(() => caches.match('./index.html'))
+      fetch(request)
+        .then(response => {
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE).then(cache => cache.put('./index.html', copy)).catch(() => {});
+          }
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
     );
     return;
   }
 
   event.respondWith(
-    fetch(request).then(response => {
-      if (response.ok) {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(request, copy)).catch(() => {});
-      }
-      return response;
-    }).catch(() => caches.match(request))
+    fetch(request)
+      .then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(request, copy)).catch(() => {});
+        }
+        return response;
+      })
+      .catch(() => caches.match(request))
   );
 });
